67c816033c2c80039fd33e8ee62d316c10591731
gabriellocp <gabriel_campista@hotmail.com>
2024-01-17 14:56:36 -0300

    chore: Configured ESlint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// eslint-disable-next-line react/no-deprecated
 ReactDOM.render(
-    <div>First render element</div>,
-    document.getElementById('root')
-)
+  <div>First render element</div>,
+  document.getElementById('root'),
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,34 @@
-const path = require('path')
+const path = require('path');
 
-const HTMLWebpackPlugin = require('html-webpack-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry: path.resolve(__dirname, 'src', 'index.js'),
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle[hash].js',
-        clean: true
-    },
-    mode: 'development',
-    module: {
-        rules: [
-            {test: /\.js$/, use: 'babel-loader', exclude: /node_modules/},
-            {
-                test: /\.scss$/, 
-                use: [
-                {loader: "style-loader"},
-                {loader: "css-loader", options: {modules: true}},
-                {
-                    loader:'sass-loader',
-                }],
-                exclude: /node_modules/}
-        ]
-    },
-    plugins: [
-        new HTMLWebpackPlugin({template: path.resolve(__dirname,'public','index.html')}),
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle[hash].js',
+    clean: true,
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader', options: { modules: true } },
+          {
+            loader: 'sass-loader',
+          }],
+        exclude: /node_modules/,
+      },
     ],
-    devServer:{
-        port: 3001
-    }
-}
+  },
+  plugins: [
+    new HTMLWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
+  ],
+  devServer: {
+    port: 3001,
+  },
+};
