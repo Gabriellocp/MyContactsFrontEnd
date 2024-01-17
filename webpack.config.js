80653138ec1f521d7e8e831e6c96d99dea366b60
gabriellocp <gabriel_campista@hotmail.com>
2024-01-17 15:37:14 -0300

    chore: Configured webpack.config to load images

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,10 @@ module.exports = {
           }],
         exclude: /node_modules/,
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
