7c4f412b08a0e9786795826042cc6ac0d1f0e603
gabriellocp <gabriel_campista@hotmail.com>
2024-01-17 16:11:48 -0300

    feat: Created ContactsList component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './styles.scss';
 import Header from '../Header';
+import ContactsList from '../ContactsList';
 
 export default function App() {
   return (
     <div className={styles.container}>
       <Header />
+      <ContactsList />
     </div>
   );
 }
---
+++ src/components/ContactsList/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './styles.scss';
+
+export default function ContactsList() {
+  return (
+    <div className={styles.container}>
+      <header>
+        <strong>3 Contacts</strong>
+        <a href="/">New contact</a>
+      </header>
+
+    </div>
+  );
+}
