0a3758cbe26139de8c1ca37725e59087f57306c4
gabriellocp <gabriel_campista@hotmail.com>
2024-01-19 14:57:29 -0300

    feat: Added list of contacts

--- src/components/ContactsList/index.js
+++ src/components/ContactsList/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import styles from './styles.scss';
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trashcan from '../../assets/images/icons/trashcan.svg';
 
 export default function ContactsList() {
   return (
@@ -8,7 +11,32 @@ export default function ContactsList() {
         <strong>3 Contacts</strong>
         <a href="/">New contact</a>
       </header>
-
+      <div className={styles.listContainer}>
+        <header>
+          <button onClick={() => {}} type="button">
+            Name
+            <img src={arrow} alt="order" />
+          </button>
+        </header>
+        <div className={styles.card}>
+          <div className={styles.info}>
+            <div className={styles.contact}>
+              <strong>Name</strong>
+              <small>Instagram</small>
+            </div>
+            <span>Email</span>
+            <span>Phone</span>
+          </div>
+          <div className={styles.actions}>
+            <a href="/">
+              <img src={edit} alt="edit" />
+            </a>
+            <button type="button" onClick={() => {}}>
+              <img src={trashcan} alt="delete" />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
