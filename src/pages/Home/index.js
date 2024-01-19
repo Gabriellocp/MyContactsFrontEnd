359d7059209f34d4b16a399b636c2f5f84772df0
gabriellocp <gabriel_campista@hotmail.com>
2024-01-19 15:18:12 -0300

    refactor: Removed search input from Header and put it into Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,37 +6,43 @@ import trashcan from '../../assets/images/icons/trashcan.svg';
 
 export default function ContactsList() {
   return (
-    <div className={styles.container}>
-      <header>
-        <strong>3 Contacts</strong>
-        <a href="/">New contact</a>
-      </header>
-      <div className={styles.listContainer}>
+    <>
+      <form className={styles.searchInput}>
+        <input type="text" placeholder="Search contact by name" />
+      </form>
+      <div className={styles.container}>
         <header>
-          <button onClick={() => {}} type="button">
-            Name
-            <img src={arrow} alt="order" />
-          </button>
+          <strong>3 Contacts</strong>
+          <a href="/">New contact</a>
         </header>
-        <div className={styles.card}>
-          <div className={styles.info}>
-            <div className={styles.contact}>
-              <strong>Name</strong>
-              <small>Instagram</small>
-            </div>
-            <span>Email</span>
-            <span>Phone</span>
-          </div>
-          <div className={styles.actions}>
-            <a href="/">
-              <img src={edit} alt="edit" />
-            </a>
-            <button type="button" onClick={() => {}}>
-              <img src={trashcan} alt="delete" />
+        <div className={styles.listContainer}>
+          <header>
+            <button onClick={() => {}} type="button">
+              Name
+              <img src={arrow} alt="order" />
             </button>
+          </header>
+          <div className={styles.card}>
+            <div className={styles.info}>
+              <div className={styles.contact}>
+                <strong>Name</strong>
+                <small>Instagram</small>
+              </div>
+              <span>Email</span>
+              <span>Phone</span>
+            </div>
+            <div className={styles.actions}>
+              <a href="/">
+                <img src={edit} alt="edit" />
+              </a>
+              <button type="button" onClick={() => {}}>
+                <img src={trashcan} alt="delete" />
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
+
   );
 }
