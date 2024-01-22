f562770c9aa7dee0c72034ed495de7e59d469eb1
gabriellocp <gabriel_campista@hotmail.com>
2024-01-22 18:06:05 -0300

    feat: Added name and email validation

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-bind */
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './styles.scss';
@@ -5,13 +6,14 @@ import FormGroup from '../FormGroup';
 import Input from '../Input';
 import Select from '../Select';
 import Button from '../Button';
+import isEmailValid from '../../utils/isEmailValid';
 
 export default function ContactForm({ buttonLabel }) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [category, setCategory] = useState('');
-
+  const [errors, setErrors] = useState([]);
   function handleSubmit(event) {
     event.preventDefault();
     console.log({
@@ -19,23 +21,50 @@ export default function ContactForm({ buttonLabel }) {
     });
   }
 
+  function handleNameChange(e) {
+    const { value } = e.target;
+    setName(value);
+    if (!value) {
+      setErrors((prevState) => [...prevState, { field: 'name', message: 'Name is required' }]);
+    } else {
+      setErrors((prevState) => [...prevState].filter((err) => err.field !== 'name'));
+    }
+  }
+
+  function handleEmailChange(e) {
+    const { value } = e.target;
+    console.log(errors);
+    setEmail(value);
+    if (!value) {
+      setErrors((prevState) => [...prevState, { field: 'email', message: 'E-mail is required' }]);
+    } else if (!isEmailValid(value)) {
+      const emailAlreadyHasError = errors.find((err) => err.field === 'email');
+      if (emailAlreadyHasError) return;
+      setErrors((prevState) => [...prevState, { field: 'email', message: 'E-mail is not valid' }]);
+    } else {
+      setErrors((prevState) => [...prevState].filter((err) => err.field !== 'email'));
+    }
+  }
+
   return (
     <form
       className={styles.container}
       onSubmit={handleSubmit}
     >
-      <FormGroup error="Invalid format">
+      <FormGroup error={errors[0]?.message}>
         <Input
           placeholder="Name"
           value={name}
-          onChange={(e) => setName(e.target.value)}
+          onChange={handleNameChange}
+          error={!!errors[0]}
         />
       </FormGroup>
-      <FormGroup>
+      <FormGroup error={errors[1]?.message}>
         <Input
           placeholder="E-mail"
           value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={handleEmailChange}
+          error={!!errors[1]}
         />
       </FormGroup>
       <FormGroup>
