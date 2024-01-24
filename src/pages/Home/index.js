5098b40586ea1c6be128f530d36b24a76988180a
gabriellocp <gabriel_campista@hotmail.com>
2024-01-24 15:46:34 -0300

    feat: Integrated Home with /contacts route

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './styles.scss';
 import arrow from '../../assets/images/icons/arrow.svg';
@@ -6,6 +6,23 @@ import edit from '../../assets/images/icons/edit.svg';
 import trashcan from '../../assets/images/icons/trashcan.svg';
 
 export default function Home() {
+  const [contacts, setContacts] = useState([]);
+  const [orderBy, setOrderBy] = useState('asc');
+  useEffect(() => {
+    fetch(`http://localhost:3000/contacts?order=${orderBy}`)
+      .then(async (response) => {
+        const json = await response.json();
+        setContacts(json);
+      })
+      .catch((err) => {
+        console.log(`Error ${err}`);
+      });
+  }, [orderBy]);
+
+  function handleToggleOrderBy() {
+    setOrderBy((prevState) => (prevState === 'asc' ? 'desc' : 'asc'));
+  }
+
   return (
     <>
       <form className={styles.searchInput}>
@@ -13,35 +30,38 @@ export default function Home() {
       </form>
       <div className={styles.container}>
         <header>
-          <strong>3 Contacts</strong>
+          <strong>{`${contacts.length} Contact${contacts.length === 1 ? 's' : ''}`}</strong>
           <Link to="/new">New contact</Link>
         </header>
-        <div className={styles.listContainer}>
-          <header>
-            <button onClick={() => {}} type="button">
-              Name
-              <img src={arrow} alt="order" />
-            </button>
-          </header>
-          <div className={styles.card}>
-            <div className={styles.info}>
-              <div className={styles.contact}>
-                <strong>Name</strong>
-                <small>Instagram</small>
+        <header className={styles.listHeader}>
+          <button onClick={handleToggleOrderBy} type="button">
+            Name
+            <img src={arrow} alt="order" order={orderBy} />
+          </button>
+        </header>
+        {
+            contacts.map((contact) => (
+              <div key={contact.id} className={styles.card}>
+                <div className={styles.info}>
+                  <div className={styles.contact}>
+                    <strong>{contact.name}</strong>
+                    {contact.category && <small>{contact.category}</small>}
+                  </div>
+                  <span>{contact.email}</span>
+                  <span>{contact.phone}</span>
+                </div>
+                <div className={styles.actions}>
+                  <Link to={`/edit/${contact.id}`}>
+                    <img src={edit} alt="edit" />
+                  </Link>
+                  <button type="button" onClick={() => {}}>
+                    <img src={trashcan} alt="delete" />
+                  </button>
+                </div>
               </div>
-              <span>Email</span>
-              <span>Phone</span>
-            </div>
-            <div className={styles.actions}>
-              <Link to="/edit/123">
-                <img src={edit} alt="edit" />
-              </Link>
-              <button type="button" onClick={() => {}}>
-                <img src={trashcan} alt="delete" />
-              </button>
-            </div>
-          </div>
-        </div>
+            ))
+          }
+
       </div>
     </>
 
