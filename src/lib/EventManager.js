89bd3ba82ed73189d324777404ab43e4bd130dc3
gabriellocp <gabriel_campista@hotmail.com>
2024-02-08 16:48:59 -0300

    refactor: Change EventManager to use Map instead of object

--- src/lib/EventManager.js
+++ src/lib/EventManager.js
@@ -1,33 +1,29 @@
 export default class EventManager {
   constructor() {
-    this.listeners = {};
+    this.listeners = new Map();
   }
 
   on(event, listener) {
-    if (!this.listeners[event]) {
-      this.listeners[event] = [];
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, []);
     }
-    this.listeners[event].push(listener);
+    this.listeners.get(event).push(listener);
   }
 
   emit(event, payload) {
-    if (!this.listeners[event]) {
+    if (!this.listeners.has(event)) {
       return;
     }
-    this.listeners[event].forEach((listener) => {
+    this.listeners.get(event).forEach((listener) => {
       listener(payload);
     });
   }
 
   remove(event, listenerToRemove) {
-    if (!this.listeners[event]) {
+    if (!this.listeners.has(event)) {
       return;
     }
     const filteredListeners = this.listeners.filter((listener) => listener !== listenerToRemove);
-    this.listeners[event] = filteredListeners;
+    this.listeners.set(event, filteredListeners);
   }
 }
-
-const toastEventManager = new EventManager();
-
-console.log(toastEventManager);
